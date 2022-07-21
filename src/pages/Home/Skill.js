b456f2c0a602decb6dc75238c3a372647bea3309
Your Name <hossainhridoy@30687@gmail.com>
2022-07-21 20:30:13 +0600

    correction react to rest api

--- src/pages/Home/Skill.js
+++ src/pages/Home/Skill.js
@@ -15,7 +15,7 @@ const Skill = () => {
                 <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">Tailwind css</button>
                 <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">DaisyUI</button>
                 <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">JavaScript</button>
-                <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">React API</button>
+                <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">Rest API</button>
                 <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">React Router</button>
                 <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">Firebase</button>
                 <button className='btn sm:btn btn-sm mx-1' data-aos="zoom-in">Firebase Hooks</button>
